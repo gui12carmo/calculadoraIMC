bc9d751e8ea7935c4966f4c2d03321409f0baf93
gui12carmo <144931450+gui12carmo@users.noreply.github.com>
2025-01-20 23:17:55 +0000

    Tela inicial editText

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,35 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar os componentes da EditText
+        // Criar uma variavel e associar o componente de UI<TextInputEditText>
+        // Recuperar botão da tela
+
+        // Colocar ação no botão
+        // Recuperar o texto digitado no edt peso
+        // Recuperar o texto digitado no edt altura
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_Calcular)
+
+        btnCalcular.setOnClickListener {
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+
+
+            println("Gui o melhor cara do mundo " + altura)
+        }
+
     }
 }
