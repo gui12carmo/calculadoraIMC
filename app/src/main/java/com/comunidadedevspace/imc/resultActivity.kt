03fccdf9992b1dc30271f997c08e80fd7affd44f
gui12carmo <144931450+gui12carmo@users.noreply.github.com>
2025-01-22 17:21:26 +0000

    Finalizada a parte de mostrar o resultado, os valores já aparecem, falta agora colocar a classificação.

--- app/src/main/java/com/comunidadedevspace/imc/resultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/resultActivity.kt
@@ -1,20 +1,29 @@
 package com.comunidadedevspace.imc
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+const val KEY_RESULT_IMC = "ResultActivity.KEY_IMC"
 
 class resultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val result = intent.getFloatExtra(KEY_RESULT_IMC, 0f)
+
+
+        val tvResult = findViewById<TextView>(R.id.tv_result)
+        val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao_label)
+
+        tvResult.text = result.toString()
+
+        println("o valor do seu IMC é de: " + result)
     }
 }
